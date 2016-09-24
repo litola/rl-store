02b0e60d240e61469152d07dd23de3cbfaf6974f
Rafael Lara <ing.lara@live.com>
2016-09-24 11:49:40 -0500

    Added model vendor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160924164750) do
+ActiveRecord::Schema.define(version: 20160924164917) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street"
@@ -65,5 +65,14 @@ ActiveRecord::Schema.define(version: 20160924164750) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "vendors", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "vendors", ["category_id"], name: "index_vendors_on_category_id"
 
 end
