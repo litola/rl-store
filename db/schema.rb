0c2960e492b693805965b87abadb9f04b5691b97
Rafael Lara <ing.lara@live.com>
2016-09-24 11:55:09 -0500

    Added scaffold cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160924165319) do
+ActiveRecord::Schema.define(version: 20160924165444) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20160924165319) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.float    "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "carts", ["user_id"], name: "index_carts_on_user_id"
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
