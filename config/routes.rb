aec28031bb614572d975b65643c96e6ecb34a597
Rafael Lara <ing.lara@live.com>
2016-09-24 15:46:19 -0500

    Added Devise gemfile and started

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :carts
   resources :products
+  root to: "products#show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
